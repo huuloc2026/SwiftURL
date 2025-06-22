ac3066e7b6e36d2a111777ad64277b64f30b70d2
JakeOnyx <huuloc2026@gmail.com>
2025-06-22 10:07:36 +0700

    add dependencies

--- internal/dependencies/dependencies.go
+++ internal/dependencies/dependencies.go
@@ -1,35 +1,35 @@
 package dependencies
 
-// import (
-// 	authHandler "github.com/huuloc2026/SwiftURL/internal/auth/delivery/http"
-// 	"github.com/huuloc2026/SwiftURL/internal/auth/repository"
-// 	"github.com/huuloc2026/SwiftURL/internal/auth/usecase"
-// 	userHandler "github.com/huuloc2026/SwiftURL/internal/shorturl/delivery/http"
-// 	"github.com/huuloc2026/SwiftURL/internal/shorturl/repository"
-// 	"github.com/huuloc2026/SwiftURL/internal/shorturl/usecase"
-// 	"github.com/huuloc2026/SwiftURL/pkg/database"
-// )
+import (
+	authHandler "github.com/huuloc2026/SwiftURL/internal/auth/delivery/http"
+	"github.com/huuloc2026/SwiftURL/internal/auth/repository"
+	"github.com/huuloc2026/SwiftURL/internal/auth/usecase"
+	userHandler "github.com/huuloc2026/SwiftURL/internal/shorturl/delivery/http"
+	"github.com/huuloc2026/SwiftURL/internal/shorturl/repository"
+	"github.com/huuloc2026/SwiftURL/internal/shorturl/usecase"
+	"github.com/huuloc2026/SwiftURL/pkg/database"
+)
 
-// type Dependencies struct {
-// 	URLHandler  *userHandler.URLHandler
-// 	AuthHandler *authHandler.AuthHandler
-// }
+type Dependencies struct {
+	URLHandler  *userHandler.URLHandler
+	AuthHandler *authHandler.AuthHandler
+}
 
-// func InitDependencies(db *database.DB) *Dependencies {
-// 	// Initialize URL dependencies
-// 	urlRepo := repository.NewShortURLRepository(db)
-// 	urlUC := usecase.NewShortURLUsecase(urlRepo)
-// 	urlHandler := http.NewURLHandler(urlUC)
+func InitDependencies(db *database.DB) *Dependencies {
+	// Initialize URL dependencies
+	urlRepo := repository.NewShortURLRepository(db)
+	urlUC := usecase.NewShortURLUsecase(urlRepo)
+	urlHandler := http.NewURLHandler(urlUC)
 
-// 	// Initialize Auth dependencies
-// 	userRepo := repository.NewUserRepository(db)
-// 	otpService := NewInMemoryOTPService()
-// 	jwtService := jwt.NewSimpleJWTService("your-secret-key")
-// 	authUC := usecase.NewAuthUsecase(userRepo, otpService, jwtService)
-// 	authHandler := http.NewAuthHandler(authUC)
+	// Initialize Auth dependencies
+	userRepo := repository.NewUserRepository(db)
+	otpService := NewInMemoryOTPService()
+	jwtService := jwt.NewSimpleJWTService("your-secret-key")
+	authUC := usecase.NewAuthUsecase(userRepo, otpService, jwtService)
+	authHandler := http.NewAuthHandler(authUC)
 
-// 	return &Dependencies{
-// 		URLHandler:  urlHandler,
-// 		AuthHandler: authHandler,
-// 	}
-// }
+	return &Dependencies{
+		URLHandler:  urlHandler,
+		AuthHandler: authHandler,
+	}
+}
